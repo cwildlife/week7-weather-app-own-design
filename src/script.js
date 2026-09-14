898cd7eabfcb51d98b83d749606ce962c54cc6ff
cwildlife <cwildlife@gmail.com>
2026-09-14 15:51:49 +0200

    Retrying JS function execution

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-alert ("Hi");
+function doTheSearch(event){
+  event.preventDefault();
+
+let SearchInput = document.querySelector("#city");
+console.log(SearchInput.value);
+let cityElement = document.querySelector("#city-input");
+cityElement.innerHTML = SearchInput.value;
+}
+
+let searchForm=document.querySelector("#city-search-button");
+searchForm.addEventListener("submit", doTheSearch);
