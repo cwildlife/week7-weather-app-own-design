9e4c4aa102950026be798435256bdde9c1bce491
cwildlife <cwildlife@gmail.com>
2026-09-14 16:50:06 +0200

    Added API

--- src/script.js
+++ src/script.js
@@ -1,6 +1,15 @@
-function searchCity(city){
+function updateWeather(response){
+  let temperatureElement=document.querySelector("#temperature-value");
+let temperature = response.data.temperature.current;
+  temperatureElement.innerHTML=response.data.temperature.current;
 
-  
+}
+
+
+function searchCity(city){
+let apiKey="b94o8b93a9f0455cftd053151d5ee87d";
+let apiUrl='https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric';
+axios.get(apiUrl).then(updateWeather);
 }
 
 
@@ -11,6 +20,7 @@ function doTheSearch(event){
     let searchInput=document.querySelector("#city-input");
     let cityElement=document.querySelector("h1");
     cityElement.innerHTML=searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement=document.querySelector("#search-form");
