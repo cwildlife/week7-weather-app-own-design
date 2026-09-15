3541014f9e3ab696afeb43a088ab28833396ae76
cwildlife <cwildlife@gmail.com>
2026-09-15 12:33:37 +0200

    Fixed bug with real-time time

--- src/script.js
+++ src/script.js
@@ -27,7 +27,7 @@ function formatDate(date){
   let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let day = days[date.getDay()];
 
-  if (minutes < 10);{
+  if (minutes < 10){
     minutes=`0${minutes}`;
   }
   return `${day} ${hours}:${minutes}`;
