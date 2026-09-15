813c821f950c3ad3b504c4d839caa34177eba42d
cwildlife <cwildlife@gmail.com>
2026-09-15 12:04:51 +0200

    Added real-time date and time

--- src/script.js
+++ src/script.js
@@ -4,18 +4,37 @@ function updateWeather(response){
   let conditionsElement=document.querySelector("#conditions-description");
   let humidityElement=document.querySelector("#humidity");
 let windSpeedElement=document.querySelector("#wind-speed");
-  
+
+let dateTimeElement=document.querySelector("#date-time");
+let date=new Date(response.data.time *1000);
+
+
+
   let cityElement=document.querySelector("#city-name");
   cityElement.innerHTML=response.data.city;
   conditionsElement.innerHTML=response.data.condition.description;
   humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML= `${response.data.wind.speed} km/h`;
   temperatureElement.innerHTML=Math.round (temperature);
+  dateTimeElement.innerHTML=formatDate(date);
 }
+
+
+function formatDate(date){
+  let minutes = date.getMinutes();
+  let hours= date.getHours();
+  let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+
+  if (minutes < 10);{
+    minutes=`0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+  }
 
 function searchCity(city){
 let apiKey="b94o8b93a9f0455cftd053151d5ee87d";
-let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 axios.get(apiUrl).then(updateWeather);
 }
 
